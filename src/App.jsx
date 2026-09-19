a687927fc25e0dccb9d3771c8383d81bd3d04296
Steve Jeffs <topjeffs@hotmail.com>
2026-09-19 21:49:55 +0100

    Add pricing data and render package list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { packages } from './data/pricing.js'
+
 function App() {
-  return <h1>Hello from Steve</h1>
+  return (
+    <div>
+      <h1>Keystone quote builder</h1>
+      <ul>
+        {packages.map((pkg) => (
+          <li key={pkg.id}>
+            {pkg.name} - £{pkg.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App
---
+++ src/data/pricing.js
@@ -0,0 +1,5 @@
+export const packages = [
+    { id: 'starter', name: 'Starter', price: 495 },
+    { id: 'standard', name: 'Standard', price: 995 },
+    { id: 'premium', name: 'Premium', price: 1750 },
+]
